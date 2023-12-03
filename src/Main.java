6ebcf5dfdcbbad8697193cdd529e3b3127186358
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-12-03 04:15:00 +0300

    add: - sprites for entities in Renderer

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import world_map.WorldMap;
 
 
 public class Main {
+
     public static void main(String[] args) {
         WorldMap map = new WorldMap(10);
         Actions actions = new Actions();
-        actions.setEntitiesOnMap(map,10);
+        actions.setHerbivoresOnMap(map,10);
     }
 }
--- src/Renderer.java
+++ src/Renderer.java
@@ -1,6 +1,13 @@
 import world_map.WorldMap;
 
 public class Renderer {
+    private static final String PREDATOR ="\033[5;38;05;196m◼";
+    private static final String HERBIVORE ="\033[38;05;226m◼";
+    private static final String ROCK = "\033[38;05;242m◼";
+    private static final String GRASS ="\033[38;05;46m◼";
+    private static final String TREE ="\033[38;05;28m◼";
+    private static final String EMPTY ="▢";
+    private static final String RESET ="\033[0m";
 
     public void printMapState(WorldMap map, int length, int width) {
 
