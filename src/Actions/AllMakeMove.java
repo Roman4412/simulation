00bba7962b242e1cfae7da6e5df176f31da172e1
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-12-26 03:07:08 +0300

    refactor:  -AllMakeMove.execute()

--- src/Actions/AllMakeMove.java
+++ src/Actions/AllMakeMove.java
@@ -3,18 +3,11 @@ package Actions;
 import entities.Creature;
 import world_map.WorldMap;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class AllMakeMove {
     public void execute(WorldMap map) {
-        List<Creature> creatures = map.getMap().values().stream()
-                .filter(a -> a instanceof Creature)
-                .map(a -> (Creature) a)
-                .collect(Collectors.toList());
-
-        for (Creature c : creatures) {
-            c.makeMove(map);
-        }
+        map.getMap().values().stream()
+                .filter(entity -> entity instanceof Creature)
+                .map(entity -> (Creature) entity)
+                .forEach(creature -> creature.makeMove(map));
     }
 }
